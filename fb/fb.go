c08927dbd6a736c2fe8776f3f3a732f2c6cd1418
minodisk <daisuke.mino@gmail.com>
2018-08-06 23:29:20 +0900

    Request scraping serially

--- fb/fb.go
+++ fb/fb.go
@@ -1,6 +1,9 @@
 package fb
 
 import (
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 )
@@ -9,14 +12,27 @@ type Client struct {
 	accessToken string
 }
 
-func NewClient(t string) *Client {
-	return &Client{t}
+func NewClient(t string) (*Client, error) {
+	fmt.Println(t)
+	if t == "" {
+		return nil, errors.New("empty access token")
+	}
+	return &Client{t}, nil
 }
 
 func (c *Client) Scrape(u string) error {
-	form := url.Values{}
-	form.Add("id", u)
-	form.Add("scrape", "true")
-	form.Add("access_token", c.accessToken)
-	return http.Post("https://graph.facebook.com", "", form)
+	fmt.Println("Scrape", u)
+	res, err := http.PostForm("https://graph.facebook.com", url.Values{"id": {u}, "scrape": {"true"}, "access_token": {c.accessToken}})
+	if err != nil {
+		return err
+	}
+	if res.StatusCode >= 300 {
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+		return errors.New(string(body))
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/minodisk/rescraper/fb"
+)
+
+var (
+	cli *fb.Client
+	m   = new(sync.Mutex)
 )
 
 type ListedError struct {
@@ -55,26 +64,75 @@ func (e Errors) Error() string {
 var cache = []string{}
 
 func main() {
+	var err error
+	cli, err = fb.NewClient(os.Getenv("FB_ACCESS_TOKEN"))
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("%s\n", err.Error()))
+		os.Exit(1)
+		return
+	}
+
 	flag.Parse()
 	root := flag.Arg(0)
 	if err := process(root); err != nil {
-		// if _, e := os.Stderr.WriteString(err.Error()); e != nil {
-		// 	panic(e)
-		// }
-		// os.Exit(1)
+		if _, e := os.Stderr.WriteString(err.Error()); e != nil {
+			panic(e)
+		}
+		os.Exit(1)
 	}
 }
 
 func process(u string) error {
-	return parallel([]string{u})
+	normalizedURL, err := normalizeURL(u)
+	if err != nil {
+		return err
+	}
+	if err := parallel([]string{normalizedURL}); err != nil {
+		return err
+	}
+
+	fmt.Println(len(cache), cache)
+
+	return nil
 }
 
-func parallel(urls []string) Errors {
+var id = 0
+
+func normalizeURL(u string) (string, error) {
+	obj, err := url.Parse(u)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s://%s%s", obj.Scheme, obj.Host, obj.RequestURI()), nil
+}
+
+func filterURLs(urls []string) ([]string, error) {
+	unlistedURLs := []string{}
+outer:
+	for _, u := range urls {
+		normalizedURL, err := normalizeURL(u)
+		if err != nil {
+			return nil, err
+		}
+		for _, listedURL := range cache {
+			if normalizedURL == listedURL {
+				// already listed
+				continue outer
+			}
+		}
+		unlistedURLs = append(unlistedURLs, normalizedURL)
+	}
+	return unlistedURLs, nil
+}
+
+func parallel(urls []string) error {
+	for _, u := range urls {
+		cache = append(cache, u)
+	}
+
 	count := 0
 	errChan := make(chan error, 1)
 	doneChan := make(chan struct{}, 1)
-
-	fmt.Println("Start:", len(urls))
 
 	for _, u := range urls {
 		count++
@@ -83,32 +141,31 @@ func parallel(urls []string) Errors {
 				doneChan <- struct{}{}
 			}()
 
-			obj, err := url.Parse(u)
-			if err != nil {
-				errChan <- err
-				return
-			}
-			validURL := fmt.Sprintf("%s://%s%s", obj.Scheme, obj.Host, obj.RequestURI())
-			for _, listedURL := range cache {
-				if validURL == listedURL {
-					errChan <- NewListedError(validURL)
-					return
-				}
-			}
-			cache = append(cache, validURL)
-
-			urls, err := scrape(u)
-			if err != nil {
-				errChan <- err
-				return
-			}
-
-			fmt.Println(u, "->", len(urls))
-
-			if errs := parallel(urls); errs != nil {
-				for _, err := range errs {
+			m.Lock()
+			go func(u string) {
+				defer m.Unlock()
+				time.Sleep(2 * time.Second)
+				if err := cli.Scrape(u); err != nil {
 					errChan <- err
 				}
+			}(u)
+
+			us, err := scrape(u)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			filteredURLs, err := filterURLs(us)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			if len(filteredURLs) == 0 {
+				return
+			}
+			if err := parallel(filteredURLs); err != nil {
+				errChan <- err
 			}
 		}(errChan, u)
 	}
@@ -128,8 +185,6 @@ loop:
 			}
 		}
 	}
-
-	fmt.Println("Finish", len(urls))
 
 	if len(errs) > 0 {
 		return errs
